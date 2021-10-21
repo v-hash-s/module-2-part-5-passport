52e0961c5a77954eeaeef978dd4c94cd5d9f70c6
vlasta_stelmakh <vlasta.stelmakh2.0@gmail.com>
2021-10-21 10:23:11 +0300

    fix: add salt rounds to .env

--- src/appLogic/hashPassword.ts
+++ src/appLogic/hashPassword.ts
@@ -1,18 +1,18 @@
 import * as bcrypt from "bcrypt";
 
 async function hashPassword(password: string) {
-  console.log("Pasword in function: ", password);
-  const saltRounds = 10;
-  const salt = await getSalt(saltRounds);
-  const hashedPassword = await bcrypt.hash(password, salt);
-  console.log("hashed password: ", hashedPassword);
-  return hashedPassword;
+    console.log("Pasword in function: ", password);
+    const saltRounds = process.env.SALT_ROUNDS;
+    const salt = await getSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log("hashed password: ", hashedPassword);
+    return hashedPassword;
 }
 
 async function getSalt(saltRounds: number) {
-  const salt = await bcrypt.genSalt(saltRounds);
-  console.log("Salt: ", salt);
-  return salt;
+    const salt = await bcrypt.genSalt(saltRounds);
+    console.log("Salt: ", salt);
+    return salt;
 }
 
 export default hashPassword;
